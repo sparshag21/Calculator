ca4371518fe6b542a9d0c778fb5a8388ebf67317
Sparsh <sparsh21agarwal@gmail.com>
2019-03-04 09:34:52 +0530

    Added code for calculator

---
+++ calc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 09:28:49 2019
+
+@author: sparsh
+"""
+
+print("1. Addition");
+print("2. Subtraction");
+print("3. Multiplication");
+print("4. Division");
+print("5. Exit");
+choice = int(input("Enter your choice: "));
+if (choice>=1 and choice<=4):
+    print("Enter two numbers: ");
+    num1 = int(input());
+    num2 = int(input());
+    if choice == 1:
+    	res = num1 + num2;
+    	print("Result = ", res);
+    elif choice == 2:
+    	res = num1 - num2;
+    	print("Result = ", res);
+    elif choice == 3:
+    	res = num1 * num2;
+    	print("Result = ", res);
+    else:
+    	res = num1 / num2;
+    	print("Result = ", res);
+elif choice == 5:
+    exit();
+else:
+    print("Wrong input..!!");
