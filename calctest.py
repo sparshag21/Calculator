0e6ac7fd36819c99565b086f020a0cdc0efdac80
Sparsh <sparsh21agarwal@gmail.com>
2019-03-07 18:49:58 +0530

    changed tests(2)

--- calctest.py
+++ calctest.py
@@ -1,48 +1,48 @@
 import calc as module 
 
-# class TestClass:
+class TestClass:
 
-#     def test_function_1(self):
-#         # Override the Python built-in input method 
-#         module.input = lambda: '4'
-#         # Call the function you would like to test (which uses input)
-#         output = module.choose()  
-#         assert output == 4
+    def test_function_1(self):
+        # Override the Python built-in input method 
+        module.input = lambda: '4'
+        # Call the function you would like to test (which uses input)
+        output = module.choose()  
+        assert output == 4
 
-#     def test_function_2(self):
-#         module.input = lambda: '3'
-#         output = module.choose()  
-#         assert not output == 4        
+    def test_function_2(self):
+        module.input = lambda: '3'
+        output = module.choose()  
+        assert not output == 4        
 
-#     def teardown_method(self, method):
-#         # This method is being called after each test case, and it will revert input back to original function
-#         module.input = input
+    def teardown_method(self, method):
+        # This method is being called after each test case, and it will revert input back to original function
+        module.input = input
 
 def test_numbers_3_4_add():
     assert module.add(3,4) == 7 
 
-def test_addfalse():
-    assert not module.add(4,4) == 2  
+# def test_addfalse():
+#     assert not module.add(4,4) == 2  
 
 def test_numbers_3_4_sub():
     assert module.sub(3,4) == -1 
 
-def test_subfalse():
-    assert not module.sub(4,4) == 2  
+# def test_subfalse():
+#     assert not module.sub(4,4) == 2  
 
 def test_numbers_3_4_multiply():
     assert module.multiply(3,4) == 12 
       
-def test_multiplyfalse():
-    assert not module.multiply(4,4) == 2  
+# def test_multiplyfalse():
+#     assert not module.multiply(4,4) == 2  
 
 def test_numbers_3_4_div():
     assert module.div(3,4) == 0.75    
 
-def test_divfalse():
-    assert not module.div(4,4) == 2    
+# def test_divfalse():
+#     assert not module.div(4,4) == 2    
 
-    # import mock
+# import mock
 
 # def test_choose():
 
